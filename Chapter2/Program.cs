706ccc41bb5a5bd5cd06012800b06f327dd47525
dedkam <dedkam@gmail.com>
2024-11-22 17:11:08 +0100

    String literal

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -9,9 +9,33 @@ internal class Program
         Console.WriteLine(p1.Name);
         Console.WriteLine(p2.Name);
         Console.WriteLine(Panda.Population);
+
+        // INFINITY VALUES
+        // Dividing a nonzero number by zero results in an infinite value:
+        Console.WriteLine(1.0 / 0.0);                  //  Infinity
+        // Dividing zero by zero, or subtracting infinity from infinity, results in a NaN:
+        Console.WriteLine(0.0 / 0.0);                  //  NaN
+
+
+        // RAW STRING LITERAL c#11
+        // Wrapping a string in three or more quote characters (""") creates a raw string literal.
+        
+        string raw = """<file path="c:\temp\test.txt"></file>""";
+        Console.WriteLine(raw);
+        //Multiline raw string literals
+        string multiLineRaw = """
+            Line 1
+            Line 2
+            """;
+
+        Console.WriteLine("""
+            {
+              "Name" : "Joe"
+            }
+            """); 
     }
 
-    
+
 }
 
 namespace Animals
