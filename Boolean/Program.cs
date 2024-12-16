01ca23a88c0a6feb180b46a620d302dcff4b14ec
dedkam <dedkam@gmail.com>
2024-12-16 16:22:26 +0100

    boolean variables

---
+++ Boolean/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Boolean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me permission level");
+            string permission = Console.ReadLine();
+            int level;
+            string levelString;
+
+            do
+            {
+                Console.WriteLine("Please insert your level :");
+                levelString = Console.ReadLine();
+            }
+            while (!int.TryParse(levelString, out level));
+               
+
+            WelcomeMethod(permission, level);
+
+
+            void WelcomeMethod(string permission, int lvl)
+            {
+                if (permission.Contains("Admin"))
+                {
+                    if (lvl > 55)
+                        Console.WriteLine("Welcome, Super Admin user");
+                    else
+                        Console.WriteLine("Welcome, Admin user");
+                }
+                else if (permission.Contains("Manager"))
+                {
+                    if (lvl > 20)
+                        Console.WriteLine("Contact an Admin for access.");
+                    else
+                        Console.WriteLine("You do not have sufficient privileges.");
+                }
+                else
+                    Console.WriteLine("You do not have sufficient privileges.");
+
+
+
+            }
+        }
+    }
+}
