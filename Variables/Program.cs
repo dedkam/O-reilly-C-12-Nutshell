ef1225977dadc56cc553d4be46270711e21426d9
dedkam <dedkam@gmail.com>
2025-01-01 20:16:10 +0100

    string formatting

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Diagnostics.Metrics;
 using System.Runtime.Intrinsics.X86;
+using System.Globalization;
 
 namespace Variables
 {
@@ -145,6 +146,105 @@ namespace Variables
             // made safely and were willing to accept truncation of values after the decimal. And you used the Convert() method when you wanted to perform a conversion
             // and use common rounding rules when performing a narrowing conversion.
             // Finally, you used the TryParse() methods when the conversion from a string to a numeric data type could potentially result in a data type conversion exception.
+
+
+            // String Formatting
+            string first2 = "Hello";
+            string second2 = "World";
+            string result5 = string.Format("{0} {1}!", first2, second2);
+            Console.WriteLine(result5);
+
+            // Interpolation
+
+            string first3 = "Hello";
+            string second3 = "World";
+            Console.WriteLine($"{first3} {second3}!");
+            Console.WriteLine($"{second3} {first3}!");
+            Console.WriteLine($"{first3} {first3} {first3}!");
+
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+            decimal price = 123.45m;
+            int discount = 50;
+            Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+            //:C currency format specifier is used to present the price and discount variables as currency. 
+
+
+            decimal measurement = 123456.78912m;
+            Console.WriteLine($"Measurement: {measurement:N} units"); // Measurement: 123,456.79 units
+            // By default, the N numeric format specifier displays only two digits after the decimal point.
+
+            // formatting percentages
+            decimal tax = .36785m;
+            Console.WriteLine($"Tax rate: {tax:P2}");
+
+            decimal price2 = 67.55m;
+            decimal salePrice = 59.99m;
+
+            string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", (price2 - salePrice), price2);
+            yourDiscount += $"A discount of {((price2 - salePrice) / price2):P2}!"; 
+
+            Console.WriteLine(yourDiscount);
+
+            int invoiceNumber = 1201;
+            decimal productShares = 25.4568m;
+            decimal subtotal = 2750.00m;
+            decimal taxPercentage = .15825m;
+            decimal total5 = 3185.19m;
+
+            Console.WriteLine($"Invoice Number: {invoiceNumber}");
+            Console.WriteLine($"\tProduct Shares: {productShares:N3}");
+            Console.WriteLine($"\tSubtotal: {subtotal:C}");
+            Console.WriteLine($"\tTax Percentage: {taxPercentage:P2}");
+            Console.WriteLine($"\tTotal: {total5:C}");
+
+
+            string input = "Pad this";
+            Console.WriteLine(input.PadLeft(12));
+            Console.WriteLine(input.PadLeft(12, '-')); //  ----Pad this
+            Console.WriteLine(input.PadRight(12, '-'));//  Pad this----
+
+            string paymentId = "769C";
+            string payeeName = "Mr. Stephen Ortega";
+            string paymentAmount = "$5,000.00";
+
+            var formattedLine = paymentId.PadRight(6);
+            formattedLine += payeeName.PadRight(24);
+            formattedLine += paymentAmount.PadLeft(10);
+            
+            Console.WriteLine(formattedLine);
+            Console.WriteLine("1234567890123456789012345678901234567890");
+
+            //Exercise
+            string customerName = "Ms. Barros";
+
+            string currentProduct = "Magic Yield";
+            int currentShares = 2975000;
+            decimal currentReturn = 0.1275m;
+            decimal currentProfit = 55000000.0m;
+
+            string newProduct = "Glorious Future";
+            decimal newReturn = 0.13125m;
+            decimal newProfit = 63000000.0m;
+
+            Console.WriteLine($"Dear {customerName},");
+            Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\r\n");
+            Console.WriteLine($"Currently, you own {currentShares:C} shares at a return of {currentProfit:P2}");
+            Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.");
+
+            Console.WriteLine("Here's a quick comparison:\n");
+
+            string comparisonMessage = "";
+
+            comparisonMessage += currentProduct.PadRight(16);
+            comparisonMessage += $"{currentReturn:P2}".PadRight(8);
+            comparisonMessage += $"{currentProfit:C}";
+            comparisonMessage += "\n";
+            comparisonMessage += newProduct.PadRight(16);
+            comparisonMessage += $"{newReturn:P2}".PadRight(8);
+            comparisonMessage += $"{newProfit:C}";
+
+            Console.WriteLine(comparisonMessage);
+
         }
     }
 }
